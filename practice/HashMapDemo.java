014dda62add95f95418c8c8ed81ca0e636b639c0
dnyanesh-k <dnyaneshwarakanke@gmail.com>
2026-08-05 16:15:10 +0000

    hashmap demo

---
+++ practice/HashMapDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class HashMapDemo{
+    public static void main(String [] args){
+        HashMap m = new HashMap();
+        m.put("100","Soham");
+        m.put("200","Dhanu");
+        m.put("300","Omkar");
+        System.out.println(m);
+    }
+}
