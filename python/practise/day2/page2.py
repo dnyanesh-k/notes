10b0b02470f55c2e08c74af82e02b12a3e23904b
dnyanesh-k <dnyaneshwarakanke@gmail.com>
2026-08-09 17:27:31 +0000

    arithmetic operations

---
+++ python/practise/day2/page2.py
@@ -0,0 +1,34 @@
+# arithmetic operations
+n1 = 10
+n2 = 20
+# addition
+print(f"{n1} + {n2} = {n1+n2}")
+# concatenation
+print(f"Hello + world = {'Hello' + ' ' + 'world'}")
+# -traction
+print(f"{n2} - {n1} = {n2-n1}")
+
+# true division
+print(f"{n2} / {n1} = {n2/n1}")
+print(f"{10} / {2} = {10/4}")
+
+# floor division
+print(f"{n2} // {n1} = {n2//n1}")
+print(f"{10} // {4} = {10//4}")
+
+# multiplication
+print(f"{n1} * {n2} = {n1 * n2}")
+
+# repeatation operation
+print(f" test * 3 = {'test' * 2}")
+print(f"-*80 = {'-'* 80}")
+print(f"5*5 = {'5' * 5}")
+
+# modulus
+print(f"10 % 4 = {10 % 3}")
+
+# power of
+print(f"Square of 5 = {5 ** 2}")
+print(f"Square of 89 = {89 ** 2}")
+print(f"Cube of 3 = {3 ** 3}")
+print(f"4th power of 2 = {2 ** 4}")
