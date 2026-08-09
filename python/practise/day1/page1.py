a89c9fc1f176aed4fdf60a24d8355ba8a557f2c1
dnyanesh-k <dnyaneshwarakanke@gmail.com>
2026-08-09 17:00:28 +0000

    day 1 python revision

---
+++ python/practise/day1/page1.py
@@ -0,0 +1,34 @@
+# create a variable 
+num = 20
+
+# read the value of num and print it on the console
+print(f"Number - {num}")
+
+# get the type of number
+print(f"Type of Number - {type(num)}")
+
+num = 12.12
+print(f"Type of Number - {type(num)}")
+
+# constant declarations
+PI = 3.14
+print(f"Value of PI - {PI}")
+print(f"Type of PI - {type(PI)}")
+
+# int 
+# float
+salary = 1000.00
+print(f"Salary - {salary} , Type - {type(salary)}")
+
+# string datatype
+name = "soham"
+print(f"Name - {name}")
+
+name = 'Om'
+print(f"Name - {name}")
+
+address = """house no - 101, dubale nivas
+ sant sopankaka colony, alandi
+ mh-412105
+"""
+print(f"Adress - {address}, Type - {type(address)}")
