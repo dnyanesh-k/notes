28644ef892ba41eb2beb12f3de34c1dd9eb7a2d6
dnyanesh-k <dnyaneshwarakanke@gmail.com>
2026-08-09 17:51:41 +0000

    assignemnt operators

---
+++ python/practise/day2/page5.py
@@ -0,0 +1,44 @@
+# assignment operators
+
+n1 = 10
+
+n2 = 20
+
+print(f"n1 = {n1}, n2 = {n2}")
+
+# n1 = n1 + n2
+n1 += n2
+print(f"n1 = {n1}")
+
+# n1 = n1 - n2
+n1 -= n2
+print(f"n1 = {n1}")
+
+# n1 = n1 * n2
+n1 *= n2
+print(f"n1 * n2 = {n1}")
+
+# n1 = n1 / n2
+n1 /= n2
+print(f"n1 / n2 ={n1}")
+
+# n1 = n1 // n2
+# n1 //= n2
+
+# n1 = n1 % n2
+# n1 %=n2
+
+# n1 = n1 ** n2
+n1 **=2
+print(f"n1**=n2 = {n1}")
+
+# &=
+# |=
+
+b1 = True
+# b1 = b1 and True
+b1 &=False
+print(f"B1 = {b1}")
+
+b1 |= True
+print(f"B1 = {b1}")
