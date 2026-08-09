66235b1a620978d56fa772acf7d63c7790632518
dnyanesh-k <dnyaneshwarakanke@gmail.com>
2026-08-09 17:33:54 +0000

    comparison operators

---
+++ python/practise/day2/page3.py
@@ -0,0 +1,11 @@
+# comparison operators
+
+n1 = 10
+n2 = 20
+
+print(f"{n1} > {n2} = {n1>n2}")
+print(f"{n1} < {n2} = {n1 < n2}")
+print(f"{n1} >= {n2} = {n1 >= n2}")
+print(f"{n1} <= {n2} = {n1 <= n2}")
+print(f"{n1} == {n2} = {n1 == n2}")
+print(f"{n1} != {n2} = {n1 != n2}")
