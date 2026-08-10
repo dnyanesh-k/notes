a1b7c8002cf759d468156155cc8e026e71d337ed
dnyanesh-k <dnyaneshwarakanke@gmail.com>
2026-08-10 17:39:58 +0000

    is even number

---
+++ python/practise/day2/page7.py
@@ -0,0 +1,22 @@
+# age1 = 10
+# age2 = 20
+
+# if age1 >=18:
+#     print("person1 is eligibale for voting")
+#     print("another statement in if block")
+# else:
+#     print("person1 is not eligible for voting") 
+#     print("another statement in else block")   
+
+# if age2 >= 18:
+    # print("person2 is eligible for voting")
+# else:
+    # print("person2 is not eligible for voting")        
+
+
+# get an input from user and check if input number is even
+num = int(input("Enter the number : "))
+if num % 2 == 0:
+    print(f"{num} is even number.")
+else:
+    print(f"{num} is NOT even number.")    
