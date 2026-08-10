cba857a4fe3f125d99d5835849bbfac789d5f68b
dnyanesh-k <dnyaneshwarakanke@gmail.com>
2026-08-10 17:31:36 +0000

    type conversion

---
+++ python/practise/day2/page6.py
@@ -0,0 +1,40 @@
+# type conversion or casting
+
+# take the input from user
+# input will be received from console
+# console can deal with only characters
+# ip will always return a string
+
+# num1 = input("Enter the first number : ")
+# print(f"Num1 = {num1} , type = {type(num1)}")
+
+# convert str to int 
+# num1_int = int(num1)
+# print(f"Num_int = {num1_int}, type = {type(num1_int)}")
+
+
+# num2 = input("Enter second number : ")
+# num2_int = int(num2)
+# print(f"num2 = {num2_int} , type = {type(num2_int)}")
+
+# print(f"{num1_int} + {num2_int} = {num1_int + num2_int}")
+
+
+# print(f"15.50 to int = {int(15.50)}")
+# print(f"'15' to int = {int('15')}")
+# print(f"True to int = {int(True)}")
+# print(f"False to int = {int(False)}")
+# print(f"None to int = {int(None)}") # Type ERROR int() argument must be a string
+
+# =============================================================================
+#   CONVERSION TO str()
+#==========================================================================
+
+# print(f"15 to string = {str(15.50)}")
+# print(f"True to  str = {str(True)}")
+# print(f"None to str = {str(None)}")
+
+# =============== CONVERSION TO bool() ===================================================
+print(f"15 to bool = {bool(0.1)}")
+print(f"'num' to bool = {bool('')}")
+print(f"None to bool ={bool(None)}")
