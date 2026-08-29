441dce0e9904fb9e86907120ca2cf69b45abbc19
dnyanesh-k <dnyaneshwarakanke@gmail.com>
2026-08-29 13:34:29 +0000

    frozenset

---
+++ python/practise/day5/page2.py
@@ -0,0 +1,8 @@
+# frozen set
+# - immutable collection of unique values
+
+def function1():
+    s1 = frozenset([10, 20, 30, 40, 50, 10, 20, 30, 40, 50])
+    print(f"s1 = {s1}, type = {type(s1)}")
+
+function1()
